ff7946753c11208682fe6186e2449443fa0082b6
Andy Nguyen <anbao.ng@gmail.com>
2018-10-19 00:49:40 -0700

    testing movie API in react component

---
+++ src/components/MovieCard.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class MovieCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggle: false,
+      response: [],
+    };
+  }
+
+  handleClick = () => {
+    this.setState({toggle: this.state.toggle ? false : true})
+  }
+
+  dataHandler = (data) => {
+    console.log(data);
+  }
+
+  componentDidMount = () => {
+    console.log('did mounted');
+    axios.get('https://data.tmsapi.com/v1.1/movies/showings', {
+      params: {
+        startDate: '2018-10-20',
+        zip: '92606',
+        dataType: 'json',
+        api_key: 'dgf69zqkhraznhf7zzdaee2r',
+      }
+    })
+    .then(resp => {
+      console.table(resp.data);
+      this.setState({response: resp.data})
+    })
+    .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      this.state.response.map(resp => {
+        return (
+          <img 
+          src = {
+            `http://developer.tmsimg.com/${resp.preferredImage.uri}?api_key=dgf69zqkhraznhf7zzdaee2r`
+          }
+          onClick={this.handleClick}
+          />
+        )
+      })
+    );
+  }
+}
+
+export default MovieCard;
